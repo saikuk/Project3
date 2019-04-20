8de88e77ce298c7da2616b698891299ff8df2c8f
Nathan Daly <dalynat@gmail.com>
2019-04-20 11:30:44 -0400

    fix

---
+++ client/build/precache-manifest.8317a56b73189fcc662e290dc5b6221d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "05d813e464175cb33fe54092203e01dc",
+    "url": "/static/media/pizzaBackground.05d813e4.jpg"
+  },
+  {
+    "revision": "953b449cb8fb175090c764f43fa43a0b",
+    "url": "/static/media/pizza.953b449c.png"
+  },
+  {
+    "revision": "6471f73ec0a74a763aca26f09f4512fd",
+    "url": "/static/media/handcrafted.6471f73e.jpg"
+  },
+  {
+    "revision": "e7ade10b321f36006d5deea22e647b67",
+    "url": "/static/media/combo.e7ade10b.jpg"
+  },
+  {
+    "revision": "786101f9e2ca398f8d0685f6b2ff21b5",
+    "url": "/static/media/brownie.786101f9.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6e1816d9abe5eaf6f3d3",
+    "url": "/static/js/main.0d1d685b.chunk.js"
+  },
+  {
+    "revision": "802ef6a48a931b833792",
+    "url": "/static/js/2.c9ab794e.chunk.js"
+  },
+  {
+    "revision": "6e1816d9abe5eaf6f3d3",
+    "url": "/static/css/main.adfccbf5.chunk.css"
+  },
+  {
+    "revision": "19dee085d2e01cf573d444c46a960417",
+    "url": "/index.html"
+  }
+];
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -2,35 +2,73 @@ import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
 
 class signup extends Component {
-    constructor() {
-      super();
-      this.handleSubmit = this.handleSubmit.bind(this);
+  state ={
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  }
+  makeRequest = e =>{
+    e.preventDefault();
+    // console.log('something')
+    var formData = new FormData();
+    formData.append("firstName", this.state.firstName)
+    formData.append("lastName", this.state.lastName)
+    formData.append("email", this.state.email)
+    formData.append("password", this.state.password)
+
+    fetch("/api/signup", {
+      method: "POST",
+      body: formData
+  })
+      console.log(formData)
     }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-      const data = new FormData(event.target);
-      
-      fetch('/api/delivery', {
-        method: 'POST',
-        body: data,
+    onChange = e =>{
+      const value = e.target.value;
+      const nameAttr = e.target.name;
+      this.setState({
+        [nameAttr]: value
       });
-    }
-  
+    }  
     render() {
       return (
         <div id="checkoutpage">
         <Navbar />
         <div className="container" >
-        <form onSubmit={this.handleSubmit}>
+
+        <form onSubmit={this.makeRequest}>
           <div><h1>Sign-Up</h1></div>
-          <input type ="text" placeholder ="First Name"/>
+          <input 
+            name ='firstName'
+            type ="text"
+            value = {this.state.firstName} 
+            onChange = {this.onChange}
+            placeholder ="First Name"
+          />
           <br />
-          <input type ="text" placeholder ="Last Name"/>
+          <input 
+            name = 'lastName'
+            type ="text"
+            value = {this.state.lastName} 
+            onChange = {this.onChange} 
+            placeholder ="Last Name"
+          />
           <br />
-          <input type ="email" placeholder ="Email"/>
+          <input 
+            name = 'email'
+            type ="email"
+            value = {this.state.email} 
+            onChange = {this.onChange} 
+            placeholder ="Email"
+          />
           <br />
-          <input type ="password" placeholder ="Password"/>
+          <input 
+            name = 'password'
+            type ="password"
+            value = {this.state.password} 
+            onChange = {this.onChange} 
+            placeholder ="Password"
+          />
           <br />
           <button>Sign Up</button> 
         </form>
@@ -38,5 +76,5 @@ class signup extends Component {
         </div>
       );
     }
-  }
+}
   export default signup;
