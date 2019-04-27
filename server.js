748507ad3f77b63e758c590c61e80e7f6a039be9
Sai Kuk <kukcheong@gmail.com>
2019-04-27 11:57:48 -0400

    Updated MongoDB connection

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project3db" ||  "mongodb://heroku_q3c9t3nd:Saikuk123@ds015334.mlab.com:15334/heroku_q3c9t3nd" );
+mongoose.connect(  "mongodb://heroku_q3c9t3nd:Saikuk123@ds015334.mlab.com:15334/heroku_q3c9t3nd"||
+process.env.MONGODB_URI ||
+"mongodb://localhost/project3db"  );
 
 // Start the API server
 app.listen(PORT, function() {
